69945b24152c926805b60f04db3371cb33a8f912
dropRole <dusan.radosavljevic82@gmail.com>
2023-04-24 17:03:23 +0200

    Authorization and authentication mechanisms implementation

--- src/actions/actions.module.ts
+++ src/actions/actions.module.ts
@@ -3,9 +3,10 @@ import { ActionsController } from './actions.controller';
 import { ActionsService } from './actions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Action } from './action.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Action])],
+  imports: [TypeOrmModule.forFeature([Action]), ConfigModule],
   controllers: [ActionsController],
   providers: [ActionsService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,25 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JWTOptions } from 'src/constants';
+import { JWTStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: JWTOptions,
+    }),
+    ConfigModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JWTStrategy],
+  exports: [JWTStrategy],
 })
 export class AuthModule {}
