2bff8cc494588904374aaec285035d77e0600d91
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2023-06-28 20:20:10 +0200

    Update pass-change.dto.ts
    Altered pass to an optional prop.

--- src/auth/dto/pass-change.dto.ts
+++ src/auth/dto/pass-change.dto.ts
@@ -1,14 +1,21 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+  IsOptional,
+} from 'class-validator';
 
 export class PassChangeDTO {
-  @ApiProperty({
+  @ApiPropertyOptional({
     type: 'string',
     minLength: 8,
     maxLength: 20,
     pattern: '/((?=.*d)|(?=.*W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/',
   })
   @IsString()
+  @IsOptional()
   @MinLength(8)
   @MaxLength(20)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
